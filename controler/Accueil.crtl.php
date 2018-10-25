3013417b138792a66cf9c2c7fb8347a121bea5b8
arthur-du <arthurduca38@gmail.com>
2018-10-25 09:54:17 +0200

    Add files via upload

--- controler/Accueil.crtl.php
+++ controler/Accueil.crtl.php
@@ -2,7 +2,13 @@
     // Partie principale
     // Inclusion du modèle
     include_once("../model/DAO.class.php");
-    if(isset($_GET['ref'])&& $_GET['ref']>11 && $_GET['ref']<28){
+    if(isset($_GET['cat']) && isset($_GET['ref'])){
+      $ref=$_GET['ref'];
+      $articles=$dao->getElemRace($_GET['cat']);
+      $prev = $dao->prevN($articles[0]->ref,12);
+      $next = $dao->next(end($articles));
+    }
+    else if(isset($_GET['ref'])&& $_GET['ref']>11 && $_GET['ref']<28){
       $ref=$_GET['ref'];
       $articles=$dao->getN($ref,12);
       $prev = $dao->prevN($articles[0]->ref,12);
@@ -20,5 +26,6 @@
       $prev = $dao->getN($ref,12);
       $next = $dao->next(end($articles));
     }
+
     include("../view/Accueil.view.php");
 ?>
