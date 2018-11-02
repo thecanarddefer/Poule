c0c337f9b55b111d7188234a832e65f5002322c6
thecanarddefer <32815841+thecanarddefer@users.noreply.github.com>
2018-11-02 16:36:56 +0100

    Add files via upload

---
+++ view/Panier.view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Panier</title>
+  </head>
+  <body>
+  <header>
+    <a href="Accueil.crtl.php"><p><img src="../view/image/logo.png"/></p></a>
+      <h1>Panier</h1>
+      <img src="../view/image/panier.jpg" alt="">
+  </header>
+  <table style="width: 400px">
+    <tr>
+		    <td colspan="4">Votre panier</td>
+	  </tr>
+	  <tr>
+		  <td>Nom</td>
+      <td>Race</td>
+		  <td>Prix </td>
+		  <td>Action</td>
+	  </tr>
+    <?php for($i=0 ;$i < $nbArticles ; $i++){?>
+    <tr>
+      <td><?=$poules[$i][0]->nom?></td>
+      <td><?php require_once("../model/DAO.class.php"); print($dao->getCat($poules[$i][0]->race)->nom)?></td>
+      <td><?=$poules[$i][0]->prix?></td>
+      <td><a href=\"".htmlspecialchars("panier.php?action=suppression&l=".rawurlencode($_SESSION['panier']['libelleProduit'][$i]))."\">XX</a></td>
+    </tr>
+  <?php } ?>
+</table>
+
+
+  </body>
+</html>
