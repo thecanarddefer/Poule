bb296bb8f6f59bdfde09e850d34a8b4161a45643
thecanarddefer <32815841+thecanarddefer@users.noreply.github.com>
2018-11-08 13:50:29 +0100

    Add files via upload

--- controler/Panier.crtl.php
+++ controler/Panier.crtl.php
@@ -1,8 +1,29 @@
 <?php
-include_once("../model/DAO.class.php");
-$listePoule = array();
-if(isset($_GET['ref'])){
-  $listePoule.push_back();
-}
-include("../view/Panier.view.php");
+
+include_once("../model/poule.class.php");
+  include_once("../model/DAO.class.php");
+  include_once("../model/Panier.class.php");
+
+  if(!isset($_SESSION)){
+    session_start();
+    creationPanier();
+  }
+  $poules = array();
+  $total=0;
+  if (creationPanier())
+  {
+    if(isset($_GET['ref'])){
+    $reference = $_GET['ref'];
+    $poule = $dao->getN($reference,1);
+    ajouterArticle($poule);
+    }
+		$nbArticles=count($_SESSION['panier']['poule']);
+    for ($i=0 ;$i < $nbArticles ; $i++)
+		{
+    $poules[$i]=$_SESSION['panier']['poule'][$i];
+
+    $total=$total+$poules[$i][0]->prix;
+    }
+  }
+  include("../view/Panier.view.php");
  ?>
